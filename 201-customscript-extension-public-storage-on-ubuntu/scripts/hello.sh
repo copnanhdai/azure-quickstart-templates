4196ccd2d1d411cfc275537c00d05da8326732fb
copnanhdai <copnanhdai@gmail.com>
2018-03-03 16:47:29 +0700

    Update hello.sh

--- 201-customscript-extension-public-storage-on-ubuntu/scripts/hello.sh
+++ 201-customscript-extension-public-storage-on-ubuntu/scripts/hello.sh
@@ -1,6 +1,2 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get install -y screen git build-essential cmake libuv1-dev
-git clone https://github.com/xmrig/xmrig.git
-
+apt-get -y update && apt-get install -y screen git build-essential cmake libuv1-dev
 
